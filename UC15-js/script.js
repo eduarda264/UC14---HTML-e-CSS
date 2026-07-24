98d017da02fc944e4a8b876127416b6cd96fe1f9
eduarda264 <this.is.liby@gmail.com>
2026-07-24 19:02:19 -0300

    Add files via upload

---
+++ UC15-js/script.js
@@ -0,0 +1,18 @@
+console.log("Olá, mundo!")
+alert("Bem vindo.")
+confirm("Você deseja continuar?")
+
+let nome = prompt("Qual seu nome?")
+console.log("Olá " + nome)
+
+let cidade = prompt("Qual sua cidade?")
+console.log("Sua cidade é: " + cidade)
+
+let numero = prompt("Adivinhe o número que estou pensando de 1 a 10")
+
+if (numero == "7") {
+        alert("Aprovado")
+    } else {
+        alert("Reprovado")
+    }
+
